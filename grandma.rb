33eaadf07cef136bb71fa98960f33bc5a810529a
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-27 17:11:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
-  elsif string != sting.upcase
+  elsif string != string.upcase
   "HUH?! SPEAK,UP SONNY!"
   end
 end
